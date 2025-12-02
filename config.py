bd70fd1d78dd8825f08541ab877e44b7387ec923
zhixuan88 <engzx-pm22@student.tarc.edu.my>
2025-12-02 13:13:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employeess.camqphzqk184.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employeess"
+custombucket = "engzx-s3-bucket"
+customregion = "us-east-1"
+
